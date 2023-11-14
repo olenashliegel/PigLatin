0efc0f5068baafa2be07bc1030048c0f409b99c0
Olena Shliegel <olenashliegel@gmail.com>
2023-11-14 14:07:16 -0800

    It will add test and functionality for pigLatin if first leters are conconants

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Test: "It will add 'way' to the end of words that begin with a vowel."
 Code: pigLatin("a");
 Expected Output: "away"
 
-Describe: pigLatin()
-
 Test: "It will add 'way' to the end of words that begin with a vowel."
 Code: pigLatin("A");
 Expected Output: "away"
@@ -17,3 +15,7 @@ Expected Output: "away away eway eway Iway"
 Test: "It will add 'way' to the end of words that begin with a vowel."
 Code: pigLatin("Acv add Ert ert Iui");
 Expected Output: "acvway addway ertway ertway iuiway"
+
+Test: "It will move all of the first consecutive consonants to the end and add "ay" to the end of words that begin with a consonant."
+Code: pigLatin("ba bba");
+Expected Output: "abay abbay"
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 function pigLatin(text) {
   const vowels = ["a", "e", "i", "o", "u"];
+  const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
   const textArray = text.toLowerCase().split(" ");
   const resultArray = [];
-  //debugger;
-  for (let i = 0; i < textArray.length; i++) {
-
-    if (vowels.includes(textArray[i].charAt(0))) {
-      resultArray.push(textArray[i] + "way");
+  textArray.forEach(function (word) {
+    if (vowels.includes(word.charAt(0))) {
+      resultArray.push(word + "way");
     }
-
-  }
+    else {
+      let resultWord = word;
+      for (let i = 0; i < word.length; i++) {
+        //  debugger;
+        if (consonants.includes(word[i])) {
+          resultWord = resultWord.concat(word[i]);
+        } else if (vowels.includes(word[i])) {
+          resultArray.push(resultWord.slice(i).concat("ay"));
+          break;
+        }
+      }
+    }
+  });
   return resultArray;
 }
 
-console.log(pigLatin("a"));
-console.log(pigLatin("A"));
-console.log(pigLatin("A a E e I"));
-console.log(pigLatin("Acv add Ert ert Iui"));
+//console.log(pigLatin("a"));
+//console.log(pigLatin("A"));
+//console.log(pigLatin("A a E e I"));
+//console.log(pigLatin("Acv add Ert ert Iui"));
+console.log(pigLatin("ba bba"));
